e675cc2a087e129c7f8aa0dab5e8d75762ac42b6
paul.rutledge <paul.rutledge@vodori.com>
2019-02-03 00:16:28 -0600

    req/res

--- test/websocket_layer/core_test.clj
+++ test/websocket_layer/core_test.clj
@@ -13,7 +13,20 @@
       (swap! received conj data))
     (h/send-> h/*client*
               {:id    (UUID/randomUUID)
-             :proto :push
-             :data  {:kind :message}})
+               :proto :push
+               :data  {:kind :message}})
     (Thread/sleep 100)
     (is (= [{:kind :message}] @received))))
+
+(deftest testing-request-response
+  (let [request  {:id    (UUID/randomUUID)
+                  :proto :request
+                  :data  {:kind :message}}
+        received (atom [])]
+    (defmethod wl/handle-request :message [data]
+      (swap! received conj data)
+      {:success true})
+    (h/send-> h/*client* request)
+    (let [response (h/<-receive h/*client*)]
+      (is (= [{:kind :message}] @received))
+      (is (= (assoc request :data {:success true}) response)))))
