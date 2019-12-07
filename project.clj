f6ff527080009ba598bf9d6d33884a874724ba9f
paul.rutledge <paul.rutledge@vodori.com>
2019-12-07 00:47:41 -0600

    Version 0.1.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.rutledgepaulv/websocket-layer "0.1.3"
+(defproject org.clojars.rutledgepaulv/websocket-layer "0.1.4-SNAPSHOT"
 
   :description
   "A layer of glue for jetty and core.async"
